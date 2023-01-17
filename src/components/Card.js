d43112cac6065baf85e6d48ef01efd62d041fd96
Grace Lee <gracelee9901@gmail.com>
2023-01-17 01:05:08 -0800

    Add styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,22 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-function Card(props) {  
+function Card(props) { 
     return (
+      <div>
         <div className="card">
-          <FontAwesomeIcon icon={faLocationDot} />
-          <span>{props.place.location}</span> 
-          <img src={props.place.imageUrl} alt="View on Google Maps"/>
-          <h1>{props.place.title}</h1>
-          <p>{props.place.startDate} - {props.place.endDate}</p>
-          <p>{props.place.description}</p>
-          <hr />
+          <img src={props.place.imageUrl}/>
+          <div className="card-content">
+            <FontAwesomeIcon className="location-icon" icon={faLocationDot} />
+            <span className="location-name">{props.place.location}</span> 
+            <a href={props.place.googleMapsUrl}>View on Google Maps</a>
+            <h1 className="location-title">{props.place.title}</h1>
+            <p className="date">{props.place.startDate} - {props.place.endDate}</p>
+            <p className="description">{props.place.description}</p>
+          </div>
         </div>
+        <hr />
+      </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
-    <div>
-      <FontAwesomeIcon icon={faEarthAmericas} /> 
-      <h1>my travel journal.</h1>
+    <div className="header">
+      <FontAwesomeIcon className="earth-icon" icon={faEarthAmericas} /> 
+      <h1 className="travel-title">my travel journal.</h1>
     </div>
   );
 }
